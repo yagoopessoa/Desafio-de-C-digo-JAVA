55e4b4cda64ef7fd0e3888e7dc0eedd89ad3b309
Yagoopessoa <Yago-Pessoa@outlook.com>
2025-01-09 20:58:14 -0300

    Resolução do Desafio

---
+++ Desafio-Codigo-Java/src/Problema.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Problema {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        int A, B, PROD;
+
+        System.out.println("Digite o valor de A: "); //recebendo o valor de A
+        A = scanner.nextInt();
+        System.out.println("Digite o valor de B: "); //recebendo o valor de B
+        B = scanner.nextInt();
+
+        PROD = A * B;
+        System.out.println("Resultado de  " + A + "*" + B + " é igual a: " + PROD);
+
+        scanner.close();
+        
+
+    }
+}
